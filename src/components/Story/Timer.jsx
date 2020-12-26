4048fe7c248c7d471dc900a4c56714bf18042de7
Bisvarup Mukherjee <bisvarup.m@flipkart.com>
2020-12-26 08:40:21 +0530

    feat: smooth animation using velocity

--- src/components/Story/Timer.jsx
+++ src/components/Story/Timer.jsx
@@ -1,47 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef } from "react";
+import velocity from "velocity-animate";
 import "./timer.scss";
 
-const REFRESH_INTERVAL = 500;
-
 export default function Timer({ timeout = 3000, onDone = () => {} }) {
-  const [progressPercent, setProgressPercent] = useState(0);
-  const [elapsedTime, setElapsedTime] = useState(0);
-
-  const resetCounters = () => {
-    setProgressPercent(0);
-    setElapsedTime(0);
-  };
+  const progressRef = useRef(null);
 
   useEffect(() => {
-    const t = setTimeout(() => {
-      if (progressPercent <= 100) {
-        setProgressPercent(Math.ceil((elapsedTime * 100) / timeout));
-        setElapsedTime(elapsedTime + REFRESH_INTERVAL);
-      }
-      if (progressPercent >= 100) {
-        console.log("reseting");
-        resetCounters();
-        onDone();
-      }
-    }, REFRESH_INTERVAL);
-
-    return () => clearInterval(t);
-  }, [
-    elapsedTime,
-    progressPercent,
-    setProgressPercent,
-    setElapsedTime,
-    timeout,
-    onDone,
-  ]);
+    velocity(
+      progressRef.current,
+      { width: "100%" },
+      { duration: timeout }
+    ).then(onDone);
+  }, [timeout, onDone]);
 
   return (
     <div className='timer'>
       <div
+        ref={progressRef}
         style={{
           height: "100%",
           background: "rgb(124, 124, 124)",
-          width: `${progressPercent}%`,
+          width: `0%`,
           transition: "width 0.1s",
         }}
       ></div>
